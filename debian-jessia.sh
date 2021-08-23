17a7998f886b15b74f5e36d907b8943dec840d39
Gugun <54890927+Gugun09@users.noreply.github.com>
2021-08-23 08:40:57 +0800

    Add files via upload

---
+++ debian-jessia.sh
@@ -0,0 +1,44 @@
+!#/bin/sh
+
+#################################################
+#     Server Configuration for Debian Final     #
+#              Don't use port 7071              #     
+#################################################
+
+## updating packages
+sudo apt-get update -y
+
+## installing dependencies
+sudo apt-get install unzip -y
+sudo apt-get install nano -y
+sudo apt-get install sed -y
+sudo apt-get install screen -y
+
+## installing HTTP Server (Powered By Apache)
+sudo apt-get install apache2 -y
+
+## starting HTTP Services
+sudo service apache2 start
+
+## fix for hostname resolving
+echo "ServerName	localhost" >> /etc/apache2/apache2.conf
+
+## install PHP5
+sudo apt-get install php5 php5-mysql php-devel php5-gd php5-pecl-memcache php5-pspell php5-snmp php-xml-rpc php-xml libapache2-mod-php5 -y
+
+## change to port 7071
+echo "Listen	7071" >> /etc/apache2/apache2.conf
+
+##restarting HTTP Services
+sudo service apache2 restart
+
+## downloading phpsysinfo
+cd /var/www/html
+rm -R *
+wget https://github.com/phpsysinfo/phpsysinfo/archive/master.zip
+unzip master.zip
+cp -r phpsysinfo-master/* /var/www
+mv phpsysinfo.ini.new phpsysinfo.ini
+
+## finishing installation
+sudo service apache2 restart
